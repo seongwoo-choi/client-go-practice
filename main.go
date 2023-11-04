4334aecb25f89259ca931f4777504f2ea2308d50
최성우 <seongwoo.choi@musinsa.com>
2023-11-04 17:44:40 +0900

    refactoring

--- main.go
+++ main.go
@@ -15,12 +15,21 @@ import (
 
 func main() {
 	var kubeconfig *string
+
+	// local 에서 실행 시 config 를 가져올 때 사용
+	// kubeconfig 경로를 지정하지 않으면 $HOME/.kube/config 로 지정
 	if home := homedir.HomeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
 		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
 	flag.Parse()
+
+	// 클러스터 내부에서 config 를 가져올 때 사용
+	// config, err := rest.InClusterConfig()
+	// if err != nil {
+	//		panic(err.Error())
+	// }
 
 	// use the current context in kubeconfig
 	config, configErr := clientcmd.BuildConfigFromFlags("", *kubeconfig)
