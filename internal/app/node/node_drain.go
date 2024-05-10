937d26b85e6637aa9046c21b21dc9b4d9d732445
최성우 <seongwoo.choi@musinsa.com>
2024-05-10 18:32:23 +0900

    nodeDrain 리팩토링

--- internal/app/node/node_drain.go
+++ internal/app/node/node_drain.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	coreV1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"os"
 	"strings"
 	"time"
@@ -40,7 +41,8 @@ func NodeDrain(clientSet *kubernetes.Clientset, percentage string) error {
 			if strings.Contains(node.Annotations["alpha.kubernetes.io/provided-node-ip"], overNode.NodeName) && node.Labels["karpenter.sh/provisioner-name"] == os.Getenv("DRAIN_NODE_LABELS") {
 				log.Info("Node Name: ", node.Name, ", instance type: ", node.Labels["beta.kubernetes.io/instance-type"], ", provisioner name: ", node.Labels["karpenter.sh/provisioner-name"])
 				if err := drainSingleNode(clientSet, node.Name); err != nil {
-					return fmt.Errorf("failed to drain node ", node.Name, ", instance type: ", node.Labels["beta.kubernetes.io/instance-type"], ", provisioner name: ", node.Labels["karpenter.sh/provisioner-name"])
+					log.Info("failed to drain node ", node.Name, ", instance type: ", node.Labels["beta.kubernetes.io/instance-type"], ", provisioner name: ", node.Labels["karpenter.sh/provisioner-name"])
+					return err
 				}
 			}
 		}
@@ -51,28 +53,26 @@ func NodeDrain(clientSet *kubernetes.Clientset, percentage string) error {
 
 // drainSingleNode 함수는 하나의 노드에 대해 cordon 및 파드 종료 작업을 수행
 func drainSingleNode(clientSet *kubernetes.Clientset, nodeName string) error {
-	log.Info("Draining node " + nodeName + "...")
 	if err := cordonNode(clientSet, nodeName); err != nil {
-		return err
+		return fmt.Errorf("failed to cordon node %s: %w", nodeName, err)
 	}
 
 	if err := evictPods(clientSet, nodeName); err != nil {
-		return err
+		return fmt.Errorf("failed to evict pods from node %s: %w", nodeName, err)
 	}
 
-	// 노드의 Instance ID 를 조회
+	if err := waitForPodsToTerminate(clientSet, nodeName); err != nil {
+		return fmt.Errorf("failed to wait for pods to terminate on node %s: %w", nodeName, err)
+	}
+
 	instanceId, err := getNodeInstanceId(clientSet, nodeName)
 	if err != nil {
-		return err
-	}
-	if instanceId == "" {
-		return fmt.Errorf("failed to get instance ID for node %s", nodeName)
+		return fmt.Errorf("failed to get instance ID for node %s: %w", nodeName, err)
 	}
 
-	// 인스턴스 ID 로 EC2 인스턴스를 종료
-	log.Info("Terminating instance " + instanceId + "...")
+	log.Infof("Terminating instance %s for node %s", instanceId, nodeName)
 	if err := terminateInstance(instanceId); err != nil {
-		return err
+		return fmt.Errorf("failed to terminate instance %s: %w", instanceId, err)
 	}
 
 	return nil
@@ -106,42 +106,92 @@ func evictPods(clientSet *kubernetes.Clientset, nodeName string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
 	defer cancel()
 
-	gracePeriod := int64(30) // 일반 삭제 시 유예 기간
+	gracePeriod := int64(20) // 일반 삭제 시 유예 기간
 	immediate := int64(0)    // 강제 삭제 시 유예 기간
 	propagationPolicy := metav1.DeletePropagationOrphan
 
-	for {
-		pods, err := clientSet.CoreV1().Pods("").List(ctx, metav1.ListOptions{
-			FieldSelector: fmt.Sprintf("spec.nodeName=%s", nodeName),
-		})
-		if err != nil {
-			return fmt.Errorf("failed to list pods in node %s", nodeName)
+	pods, err := getNonCriticalPods(clientSet, nodeName)
+	if err != nil {
+		return fmt.Errorf("failed to get non-critical pods for eviction from node %s: %v", nodeName, err)
+	}
+
+	for _, pod := range pods {
+		grace := &gracePeriod
+		if shouldForceDelete(pod) {
+			grace = &immediate
 		}
 
-		if len(pods.Items) == 0 {
-			log.Info("All pods in node ", nodeName, " have been successfully evicted.")
+		log.Infof("Attempting to delete pod %s from node %s with a grace period of %d seconds", pod.Name, nodeName, *grace)
+		err := clientSet.CoreV1().Pods(pod.Namespace).Delete(ctx, pod.Name, metav1.DeleteOptions{
+			GracePeriodSeconds: grace,
+			PropagationPolicy:  &propagationPolicy,
+		})
+
+		if err != nil {
+			if errors.IsNotFound(err) {
+				log.Infof("Pod %s already deleted from node %s", pod.Name, nodeName)
+				continue
+			}
+			return fmt.Errorf("failed to delete pod %s from node %s: %v", pod.Name, nodeName, err)
+		}
+	}
+
+	log.Infof("Completed evicting pods from node %s", nodeName)
+	return nil
+}
+
+func waitForPodsToTerminate(clientSet kubernetes.Interface, nodeName string) error {
+	log.Infof("Waiting for all non-critical pods to terminate on node %s", nodeName)
+
+	_, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
+	defer cancel()
+
+	for {
+		pods, err := getNonCriticalPods(clientSet, nodeName)
+		if err != nil {
+			return fmt.Errorf("failed to get non-critical pods for eviction from node %s: %v", nodeName, err)
+		}
+
+		if len(pods) == 0 {
+			log.Infof("All non-critical pods have been terminated on node %s", nodeName)
 			return nil
 		}
 
-		for _, pod := range pods.Items {
-			if !isManagedByDaemonSetOrStatefulSet(pod) {
-				grace := &gracePeriod
-				if shouldForceDelete(pod) {
-					grace = &immediate
-				}
-				log.Infof("Deleting pod %s from node %s with grace period of %d seconds", pod.Name, nodeName, *grace)
-				if err := clientSet.CoreV1().Pods(pod.Namespace).Delete(ctx, pod.Name, metav1.DeleteOptions{
-					GracePeriodSeconds: grace,
-					PropagationPolicy:  &propagationPolicy,
-				}); err != nil {
-					return fmt.Errorf("failed to delete pod %s from node %s", pod.Name, nodeName)
-				}
+		log.Infof("Still waiting for %d pods to terminate on node %s", len(pods), nodeName)
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func getNonCriticalPods(clientSet kubernetes.Interface, nodeName string) ([]coreV1.Pod, error) {
+	podList, err := clientSet.CoreV1().Pods("").List(context.Background(), metav1.ListOptions{
+		FieldSelector: fmt.Sprintf("spec.nodeName=%s,status.phase!=Succeeded,status.phase!=Failed", nodeName),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list pods on node %s: %v", nodeName, err)
+	}
+
+	var pods []coreV1.Pod
+	for _, pod := range podList.Items {
+		if !isManagedByDaemonSetOrStatefulSet(pod) {
+			if !isCriticalPod(pod) {
+				pods = append(pods, pod)
 			}
 		}
-
-		log.Info("Waiting for pods to be terminated...")
-		time.Sleep(2 * time.Second)
 	}
+
+	return pods, nil
+}
+
+func isCriticalPod(pod coreV1.Pod) bool {
+	if pod.Namespace == "kube-system" {
+		return true
+	}
+
+	if _, exists := pod.Annotations["scheduler.alpha.kubernetes.io/critical-pod"]; exists {
+		return true
+	}
+
+	return false
 }
 
 func shouldForceDelete(pod coreV1.Pod) bool {
@@ -185,16 +235,24 @@ func terminateInstance(instanceId string) error {
 	return nil
 }
 
-func getNodeInstanceId(clientSet *kubernetes.Clientset, nodeName string) (string, error) {
+func getNodeInstanceId(clientSet kubernetes.Interface, nodeName string) (string, error) {
 	node, err := clientSet.CoreV1().Nodes().Get(context.Background(), nodeName, metav1.GetOptions{})
 	if err != nil {
+		log.WithError(err).Errorf("Failed to get node %s", nodeName)
 		return "", err
 	}
 
-	for _, addr := range node.Status.Addresses {
-		if addr.Type == "InternalIP" {
-			return addr.Address, nil
+	// 로깅으로 메타데이터 확인
+	log.Infof("Node %s providerID: %s", nodeName, node.Spec.ProviderID)
+
+	// EC2 인스턴스 ID 추출
+	if strings.HasPrefix(node.Spec.ProviderID, "aws:///") {
+		splitId := strings.Split(node.Spec.ProviderID, "/")
+		if len(splitId) >= 5 {
+			instanceId := splitId[4]
+			return instanceId, nil
 		}
 	}
-	return "", fmt.Errorf("failed to get internal IP for node %s", nodeName)
+
+	return "", fmt.Errorf("failed to parse instance ID from node %s: providerID %s is malformed", nodeName, node.Spec.ProviderID)
 }
